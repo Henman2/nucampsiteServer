bd9c7156a090ad3705b9464e1c4f5de08f17c25d
Henman2 <47754370+Henman2@users.noreply.github.com>
2024-02-26 15:36:42 +0000

    File Upload

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const usersRouter = require('./routes/usersRouter');
 const campsiteRouter = require('./routes/campsiteRouter'); 
 const promotionRouter = require('./routes/promotionRouter');
 const partnerRouter = require('./routes/partnerRouter');
+const uploadRouter = require('./routes/uploadRouter');
 const url = config.mongoUrl;
 //Connect to database
 mongoose.connect(url).then(()=>{
@@ -68,6 +69,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/campsites', campsiteRouter);
 app.use('/promotions', promotionRouter);
 app.use('/partners', partnerRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
